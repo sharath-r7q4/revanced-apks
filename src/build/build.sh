93b5c246b68be5098ac5f3d65306a70128a54dd2
Sharath <sharathsukash@gmail.com>
2026-05-09 13:21:39 +0530

    Update build.sh

--- src/build/build.sh
+++ src/build/build.sh
@@ -1,8 +1,8 @@
 source src/build/utils.sh
-# repo owner pkgname appname regex
+# repo owner pkgname appname regex method
 dl_gh "morphe-cli" "MorpheApp" "latest"
 dl_gh $1 $2 "prerelease"
 get_patches_key $4
-get_apk $3 $4 $4 $5 "Bundle_extract"
+get_apk $3 $4 $4 $5 $6
 split_editor $4 $4
 patch $4 "$2-$1" "morphe"
